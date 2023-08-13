b2c08bc695f1654d17f887a954711b7434fd1d6f
ステラ <live.hylia@outlook.com>
2023-08-13 12:42:42 -0400

    (examples) container_example.cpp

---
+++ examples/container_example.cpp
@@ -0,0 +1,11 @@
+#include "uwu_defines.h"
+
+int main() {
+    vectow<int> nums = { 1, 2, 3, 4, 5 };
+    wog owo "Vector:";
+    uwu_fow_each(nums, num) {
+        wog owo " " owo num;
+    }
+    wog owo endw;
+    w_w_wetuwn 0;
+}
