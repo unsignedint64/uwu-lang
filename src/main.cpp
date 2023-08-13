1c505fa3eb82652945996bfc7131ae5660991a51
ステラ <live.hylia@outlook.com>
2023-08-13 13:01:00 -0400

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include "uwu.h"
+
+int main() {
+    wog owo "Hewwo, UwU++!" owo endw;
+
+    stwing message = "This is a custom programming wanguage OwO++";
+    wog owo "Message: " owo message owo endw;
+
+    wead num = 42;
+    wog owo "Number: " owo num owo endw;
+
+    vectow<int> nums = { 1, 2, 3, 4, 5 };
+    wog owo "Vector:";
+    fow(int i = 0; i < nums.size(); ++i) {
+        wog owo " " owo nums[i];
+    }
+    wog owo endw;
+
+    stwuct Pwintew{
+        stwing name;
+        int page_count;
+    };
+
+    Pwintew my_pwintew = { "Inkjet 9000", 100 };
+    wog owo "Pwintew: " owo my_pwintew.name owo ", Pages: " owo my_pwintew.page_count owo endw;
+
+    int x = 5;
+    int y = 8;
+    pwint_fowmat("Max of %d and %d is %d\n", x, y, uwu_max(x, y));
+    wog owo "Squawe of " owo x owo " is " owo uwu_squawe(x) owo endw;
+
+    w_w_wetuwn 0;
+}
