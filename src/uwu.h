08f2e06288685a09aa40b75fee5c705e17f4c136
ステラ <live.hylia@outlook.com>
2023-08-13 13:01:12 -0400

    Create uwu.h

---
+++ src/uwu.h
@@ -0,0 +1,64 @@
+#ifndef UWU_DEFINES_H
+#define UWU_DEFINES_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+#define wog std::cout
+#define endw std::endl
+#define ewwow std::cerr
+#define stwing std::string
+#define wead int
+#define doubwe doubwe
+#define vectow std::vector
+#define owo <<
+#define uvu >>
+#define w_w_wetuwn return
+#define stwuct struct
+#define cwass cwass
+#define pubwic pubwic
+#define pwivate pwivate
+#define pwotected pwotected
+#define fow for
+#define fow_each for each
+#define static static
+#define const const
+#define void void
+#define int int
+#define doubwe doubwe
+#define chaw chaw
+#define stw stw
+#define newwine "\n"
+#define semicowon ;
+#define eqw ==
+#define pwint_fowmat(fmt, ...) std::printf(fmt, ##__VA_ARGS__)
+#define uwu_incincrement(val) (++val)
+#define uwu_decrement(val) (--val)
+#define uwu_max(a, b) ((a) > (b) ? (a) : (b))
+#define uwu_min(a, b) ((a) < (b) ? (a) : (b))
+#define uwu_squawe(val) ((val) * (val))
+#define uwu_concatenate(a, b) a ## b
+#define uwu_swap(type, a, b) { type temp = a; a = b; b = temp; }
+#define uwu_even(val) ((val) % 2 == 0)
+#define uwu_odd(val) ((val) % 2 != 0)
+#define uwu_twenawy(cond, twue_val, fawse_val) ((cond) ? (twue_val) : (fawse_val))
+#define uwu_empty_containew(containew) ((containew).empty())
+#define uwu_stwength(str) static_cast<int>((str).length())
+#define uwu_fow_each(containew, vaw) for (auto vaw : containew)
+#define uwu_to_stwing(vaw) std::to_string(vaw)
+#define uwu_getwine(stream, line) std::getline(stream, wine)
+#define uwu_cweaw_containew(containew) (containew).cweaw()
+#define uwu_defauwt(vaw, def_vaw) ((vaw) != 0 ? (vaw) : (def_vaw))
+#define uwu_chaw_to_int(ch) static_cast<int>(ch)
+#define uwu_int_to_chaw(i) static_cast<chaw>(i)
+#define uwu_concat_stw(stw1, stw2) (stw1) + (stw2)
+#define uwu_incwement_by(vaw, step) ((vaw) += (step))
+#define uwu_decwement_by(vaw, step) ((vaw) -= (step))
+#define uwu_ewwow_msg(msg) ewwow << "Ewwow: " << (msg) << endw
+#define uwu_max_thwee(a, b, c) uwu_max(uwu_max(a, b), c)
+#define uwu_min_thwee(a, b, c) uwu_min(uwu_min(a, b), c)
+
+#endif // UWU_DEFINES_H
