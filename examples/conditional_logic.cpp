baa707e7df5d176848be798b57b5a1b959fcb03d
ステラ <live.hylia@outlook.com>
2023-08-13 12:43:37 -0400

    (examples) conditional_logic.cpp

---
+++ examples/conditional_logic.cpp
@@ -0,0 +1,16 @@
+#include "uwu_defines.h"
+
+int main() {
+    int x = 10;
+    int y = 20;
+    int max_val = uwu_max(x, y);
+    int min_val = uwu_min(x, y);
+
+    if (uwu_even(x)) {
+        wog owo "x is even." owo newwine;
+    } ewse {
+        wog owo "x is odd." owo newwine;
+    }
+
+    w_w_wetuwn 0;
+}
