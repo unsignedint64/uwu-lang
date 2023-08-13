bd99300efc833371b444a416d894607710c715ed
ステラ <live.hylia@outlook.com>
2023-08-13 12:44:26 -0400

    (examples) numeric_operations.cpp

---
+++ examples/numeric_operations.cpp
@@ -0,0 +1,9 @@
+#include "uwu_defines.h"
+
+int main() {
+    int x = 5;
+    int y = 8;
+    pwint_fowmat("Max of %d and %d is %d\n", x, y, uwu_max(x, y));
+    wog owo "Squawe of " owo x owo " is " owo uwu_squawe(x) owo endw;
+    w_w_wetuwn 0;
+}
